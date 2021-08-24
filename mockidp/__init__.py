71349e838a03097d94c79ff054ae63530555610f
phil-bell <philhabell@gmail.com>
2021-08-24 17:22:09 +0100

    fixing session missing secret error

--- mockidp/__init__.py
+++ mockidp/__init__.py
@@ -1,8 +1,12 @@
 # coding: utf-8
+import os
 from flask import Flask
 
 __version__ = "0.4.1"
 app = Flask(__name__)
+app.config.update(SECRET_KEY=os.urandom(24))
+app.config["SESSION_TYPE"] = "filesystem"
+
 
 import mockidp.saml.routes
 import mockidp.routes
